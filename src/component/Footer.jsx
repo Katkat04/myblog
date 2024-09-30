21436c7236394bf8e556aa08fb40065139d362e0
Katy <jdiazk@uninorte.edu.co>
2024-09-29 20:31:50 -0500

    Add some changes to the footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,19 +5,20 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 function Footer() {
     return(
         <footer id= "contact" className="bg-[#181d20]  justify-center p-4">
-            <div className="flex justify-between">
-                <a className='text-xl font-bold text-white hover:text-[#7f69a5]'>
-                <FontAwesomeIcon icon={faInstagram} />
-                </a>
-                <a className='text-xl font-bold text-white hover:text-[#7f69a5]'>
-                <FontAwesomeIcon icon={faGithub}/>
-                </a>
-                <a className='text-xl font-bold text-white hover:text-[#7f69a5]'>
-                <FontAwesomeIcon icon={faLinkedin}/>
-                </a>
-            </div>
+            
             <div className='flex justify-center mt-4'>
                 <p className='text-white'>© 2024 Katy Diaz. All rights reserved. </p>
+            </div>
+            <div className="flex justify-center">
+                <a href="https://www.instagram.com/katydiazbel/" className='text-xl font-bold text-white hover:text-[#7f69a5] p-2'>
+                <FontAwesomeIcon icon={faInstagram} />
+                </a>
+                <a href="https://github.com/Katkat04" className='text-xl font-bold text-white hover:text-[#7f69a5] p-2'>
+                <FontAwesomeIcon icon={faGithub}/>
+                </a>
+                <a href="https://www.linkedin.com/in/kdiaz11/" className='text-xl font-bold text-white hover:text-[#7f69a5] p-2'>
+                <FontAwesomeIcon icon={faLinkedin}/>
+                </a>
             </div>
         </footer>
     )
